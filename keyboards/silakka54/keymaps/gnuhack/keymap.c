579d981400b09ab272b0ba792bc4b40df148db10
Carlos Pajuelo Rojo <carlos@T450>
2026-08-03 19:15:19 +0200

    Añadido ralt en capa ext

--- keyboards/silakka54/keymaps/gnuhack/keymap.c
+++ keyboards/silakka54/keymaps/gnuhack/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ES_LBRC, ES_RBRC, ES_LCBR, ES_RCBR, ES_LPRN, ES_RPRN,                             KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_TRNS,
         ES_IEXL, ES_EXLM, ES_IQUE, ES_QUES, ES_AT,   ES_EQL,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ES_TILD, ES_CCED,
         QK_REP,  ES_PLUS, ES_ASTR, ES_SCLN, ES_MORD,ES_QUOT,                             ES_BSLS, ES_PIPE, ES_LABK, ES_RABK, ES_DQUO, QK_REP,
-                                            KC_TRNS, KC_TAB, KC_ESC,            KC_TRNS, KC_BSPC, KC_TRNS
+                                             KC_TRNS, KC_TAB, KC_ESC,            KC_TRNS, OSM(MOD_RALT), KC_TRNS
     ),
 
     [_FN] = LAYOUT(
